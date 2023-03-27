2664bf3764ebafd5017f55c793d19b5f86998834
oscarg10 <oscar_andres16@hotmail.com>
2023-03-27 23:35:43 +0000

    update seed data to match my_handle

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,10 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Oscar Gordillo', 'oscargordillo' , 'oscar@hotmail.com','MOCK'),
-  ('Andrew Bayko', 'bayko' ,'test@email', 'MOCK'),
-  ('Andrew Brown', 'andrewbrown' ,'andrewbrown@exampro.com', 'MOCK');
+  ('Oscar Gordillo', 'oscargordillo' , 'oscar_andres16@hotmail.com','fc6c8353-7b8d-4d14-87a7-4219496d576b'),
+  ('Andrew Brown', 'andrewbrown' ,'andrewbrown@exampro.com', 'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'test@email', 'MOCK')
+  ;
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
