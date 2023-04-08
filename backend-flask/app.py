719be77ab65f77b8de19253a429fa9550edbf7ea
oscarg10 <oscar_andres16@hotmail.com>
2023-04-08 17:16:21 +0000

    Add return results to user_short

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -285,6 +285,7 @@ def data_activities_reply(activity_uuid):
 @app.route("/api/users/@<string:handle>/short", methods=['GET'])
 def data_users_short(handle):
   data = UsersShort.run(handle)
+
   return data, 200
 
 if __name__ == "__main__":
