6c997edb524c4d4b332fb381f41e07c4e7663a9b
oscarg10 <oscar_andres16@hotmail.com>
2023-06-11 20:43:53 +0000

    update function.rb and replaced username in DesktopNavigation

--- frontend-react-js/src/components/DesktopNavigation.js
+++ frontend-react-js/src/components/DesktopNavigation.js
@@ -25,7 +25,7 @@ export default function DesktopNavigation(props) {
       handle="messages" 
       active={props.active} />
     profileLink = <DesktopNavigationLink 
-      url="/@andrewbrown" 
+      url="/@oscargordillo" 
       name="Profile"
       handle="profile"
       active={props.active} />
